f268081b2060a927e078628e9634dcb4e1d81ec4
Jax-Painter <henderkian@gmail.com>
2026-07-02 17:58:42 +0200

    Add event handlers for bot to function properly

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,22 +1,24 @@
-const { InteractionType } = require('discord.js');
-
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client) {
     if (!interaction.isChatInputCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
-    if (!command) return;
+
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
 
     try {
       await command.execute(interaction);
     } catch (error) {
       console.error(error);
-      await interaction.reply({
-        content: 'There was an error executing this command!',
-        ephemeral: true,
-      });
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+      } else {
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
     }
   },
 };
-
--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    console.log(`✓ Bot logged in as ${client.user.tag}`);
-    client.user.setActivity('ERLC Servers', { type: 'WATCHING' });
+    console.log(`✓ Bot is ready! Logged in as ${client.user.tag}`);
+    client.user.setStatus('online');
+    client.user.setActivity('ERLC servers', { type: 'WATCHING' });
   },
 };
-
